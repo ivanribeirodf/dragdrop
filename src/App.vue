bd80c21b2252edeef5154eb5b6ffbaf67f394be7
IvanPix <ivan@pix.com.br>
2023-06-27 17:48:38 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { VueFlow, useVueFlow } from '@vue-flow/core'
+import { nextTick, watch } from 'vue'
+import SideBar from './components/Side-Bar.vue';
+import './styles/main.css'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+let id = 0
+function getId() {
+  return `dndnode_${id++}`
+}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef } = useVueFlow({
+  nodes: [
+    {
+      id: '1',
+      type: 'input',
+      label: 'input node',
+      position: { x: 250, y: 25 },
+    },
+  ],
+})
+
+function onDragOver(event) {
+  event.preventDefault()
+
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = 'move'
   }
+}
+
+onConnect((params) => addEdges(params))
+
+function onDrop(event) {
+  const type = event.dataTransfer?.getData('application/vueflow')
+
+  const { left, top } = vueFlowRef.value.getBoundingClientRect()
+
+  const position = project({
+    x: event.clientX - left,
+    y: event.clientY - top,
+  })
+
+  const newNode = {
+    id: getId(),
+    type,
+    position,
+    label: `${type} node`,
+  }
+
+  addNodes([newNode])
+
+  // align node position after drop, so it's centered to the mouse
+  nextTick(() => {
+    const node = findNode(newNode.id)
+    const stop = watch(
+      () => node.dimensions,
+      (dimensions) => {
+        if (dimensions.width > 0 && dimensions.height > 0) {
+          node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 }
+          stop()
+        }
+      },
+      { deep: true, flush: 'post' },
+    )
+  })
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div class="dndflow" @drop="onDrop">
+    <VueFlow @dragover="onDragOver" />
+
+    <SideBar />
+  </div>
+</template>
