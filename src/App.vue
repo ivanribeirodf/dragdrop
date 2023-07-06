5cd9b61ac27591aa0b2fc17e8adafb0b446ad8b0
charles <charles.bz@gmail.com>
2023-07-06 09:42:57 -0300

    Estudo api dragdrop

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <script setup>
-import { VueFlow, useVueFlow } from '@vue-flow/core'
-import { nextTick, watch } from 'vue'
-import SideBar from './components/Side-Bar.vue';
+import { VueFlow, useVueFlow, Position, MarkerType  } from '@vue-flow/core'
+import { nextTick, watch,  } from 'vue'
+import { Background, Controls,  MiniMap } from '@vue-flow/additional-components'
+
 import './styles/main.css'
+import ToolbarNode from './components/ToolbarNode.vue'
+import PanelButton  from './components/PanelButton.vue'
+import SideBar from './components/Side-Bar.vue'
+import ControlJobs from './components/ControlsJobs.vue'
+
 
 let id = 0
 function getId() {
   return `dndnode_${id++}`
 }
 
-const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef } = useVueFlow({
-  nodes: [
-    {
-      id: '1',
-      type: 'input',
-      label: 'input node',
-      position: { x: 250, y: 25 },
-    },
-  ],
+const defaultNodeStyle = {
+  border: '1px solid #10b981',
+  background: '#9400D3',
+  color: 'white',
+  borderRadius: '99px',
+}
+
+const { findNode, onConnect, addEdges, addNodes, project, vueFlowRef} = useVueFlow({
+  // nodes: [
+  //     {
+  //         id: '1',
+  //         type: 'smoothstep',
+  //         label: 'teste',
+  //         data: { toolbarPosition: Position.Top },
+  //         position: { x: 200, y: 0 },
+  //         class: 'light',
+  //         animated: true
+  //         //style: defaultNodeStyle,
+        
+  //     },
+  //   ],
 })
 
 function onDragOver(event) {
@@ -30,9 +48,10 @@ function onDragOver(event) {
 
 onConnect((params) => addEdges(params))
 
+
 function onDrop(event) {
   const type = event.dataTransfer?.getData('application/vueflow')
-
+  console.log(type)
   const { left, top } = vueFlowRef.value.getBoundingClientRect()
 
   const position = project({
@@ -42,13 +61,16 @@ function onDrop(event) {
 
   const newNode = {
     id: getId(),
-    type,
-    position,
-    label: `${type} node`,
+    type: 'toolbar',
+    position,    
+    label: `${type} node`,       
+    data: { toolbarPosition: Position.Top },    
+    style: defaultNodeStyle,
+       
   }
 
   addNodes([newNode])
-
+  
   // align node position after drop, so it's centered to the mouse
   nextTick(() => {
     const node = findNode(newNode.id)
@@ -64,12 +86,31 @@ function onDrop(event) {
     )
   })
 }
+
 </script>
 
 <template>
   <div class="dndflow" @drop="onDrop">
-    <VueFlow @dragover="onDragOver" />
+    <VueFlow @dragover="onDragOver" 
+             class="basicflow" 
+             :class="{ dark }" 
+             :default-edge-options="{ type: 'smoothstep',  markerEnd: MarkerType.Arrow }"
+             fit-view-on-init>
+     
+      <template #node-toolbar="nodeProps">
+        <ToolbarNode :data="nodeProps.data" :label="nodeProps.label" />
+      </template> 
+
+      <MiniMap />
+      <Controls />
+      <ControlJobs />
+      
+      <PanelButton /> 
+      
+      <Background :pattern-color="dark ? '#FFFFFB' : '#aaa'" gap="8" /> 
+    </VueFlow>
 
     <SideBar />
-  </div>
+    
+  </div>  
 </template>
